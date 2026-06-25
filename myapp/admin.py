9b30bb308072108fffcd5f81776ac54982331f8f
revathikuttan03 <revathikuttan03@gmail.com>
2026-06-25 10:24:50 +0530

    templates,home.html

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from myapp.models import TaskList
+
 # Register your models here.
+admin.site.register(TaskList)
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from myapp.models import TaskList
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("hello word")
+    data = TaskList.objects.all()
+    return render(request,"home.html",{"datas":data})
